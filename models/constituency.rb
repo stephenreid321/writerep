51459bac12f6e61658f1d94e624402711e83e481
postscript07@gmail.com <postscript07@gmail.com>
2016-12-07 10:36:29 +0000

    .

--- models/constituency.rb
+++ models/constituency.rb
@@ -6,11 +6,14 @@ class Constituency
   field :type, :type => String
   
   validates_presence_of :name, :type
+  
+  has_many :representatives, :dependent => :destroy
         
   def self.admin_fields
     {
       :name => :text,
-      :type => :text
+      :type => :text,
+      :representatives => :collection
     }
   end
     
