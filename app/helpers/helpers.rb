82f5611c80216ab7fe317e48f63ecd326d853aeb
postscript07@gmail.com <postscript07@gmail.com>
2016-12-08 15:16:31 +0000

    .

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -6,7 +6,7 @@ ActivateApp::App.helpers do
   
   def next_action(current_action: uri.split('/').last)
     next_action = @campaign.action_order_a[@campaign.action_order_a.index(current_action)+1]
-    next_action ? redirect("/campaigns/#{@campaign.slug}?action=#{next_action}&name=#{@resource.from_name}&email=#{@resource.from_email}&address1=#{@resource.from_address1}&postcode=#{@resource.from_postcode}") : redirect("/campaigns/#{@campaign.slug}/thanks")  
+    next_action ? redirect("/campaigns/#{@campaign.slug}?action=#{next_action}&name=#{@resource.try(:from_name)}&email=#{@resource.try(:from_email)}&address1=#{@resource.try(:from_address1)}&postcode=#{@resource.try(:from_postcode)}") : redirect("/campaigns/#{@campaign.slug}/thanks")  
   end
    
   def sign_in_required!
